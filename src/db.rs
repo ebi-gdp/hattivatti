250338bd95f6b74218039d82c1662e70c56ee3bf
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-03-26 11:21:00 +0000

    start python project

--- src/db.rs
+++
@@ -1,7 +0,0 @@
-//! All job state is stored in a SQLite database
-
-/// Connect to a SQLite database
-pub mod open;
-pub mod job;
-/// Stream and validate job request messages
-pub mod ingest;
--- src/db/job.rs
+++
@@ -1,9 +0,0 @@
-//! Job loading, updating, and submission to SLURM scheduler
-//!
-//! Takes care of deserialising unsubmitted jobs from the database into a JobRequest.
-//! Also responsible for updating the database once JobRequests are staged (rendered templates written
-//! to disk) or submitted (sbatch system command run).
-
-pub mod load;
-pub mod update;
-pub mod state;
