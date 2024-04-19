87c4e68ced401a95d1b6d44b8f593fefbe58eec1
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-04-19 16:59:41 +0100

    add more configuration

--- pyvatti/src/pyvatti/config.py
+++ pyvatti/src/pyvatti/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import Field, DirectoryPath
 from pydantic_settings import BaseSettings
 
@@ -11,6 +13,14 @@ class Settings(BaseSettings):
         default=60 * 60 * 24,
         description="Number of seconds before active (requested, created, "
         "or deployed) jobs are transitioned to FAILED state",
+    )
+    TOWER_TOKEN: str = Field(description="Seqera platform token")
+    TOWER_WORKSPACE: str = Field(description="Seqera platform workspace ID")
+    GCP_PROJECT: Optional[str] = Field(
+        default=None, description="Google Cloud Platform (GCP) project ID"
+    )
+    GCP_LOCATION: Optional[str] = Field(
+        default=None, description="Location to request GCP resources from"
     )
 
 
